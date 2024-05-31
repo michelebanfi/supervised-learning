0660c48504f118bcb2ce6a685f1973f4aa917950
Annalisa Di Pasquali <annalisadipasquali@MacBook-Pro-di-Annalisa.local>
2024-05-31 18:11:41 +0200

    Preprocessing

--- prepocess.py
+++ prepocess.py
@@ -1,4 +1,8 @@
 import torch
+import cv2
+import glob  # Import library for finding all files matching a pattern
+from PIL import Image  # Import library for image processing
+import os  # Import library for operating system functionalities
 
 train_on_gpu = torch.cuda.is_available()
 if not train_on_gpu:
@@ -12,3 +16,20 @@ trainDirectory = 'Data/train_set'
 testDirectory = 'Data/val_set'
 
 processedTrainDirectory = 'Data/processedData/processed_train_set'
+processedTestDirectory = 'Data/processedData/processed_test_set'
+
+# Define a path pattern to search for all jpg images within subdirectories of "/content/imdb_crop"
+image_path_pattern = trainDirectory +"/*.jpg"
+image_paths = glob.glob(image_path_pattern)
+
+for image_path in image_paths:
+    image = cv2.imread(image_path)
+
+    if (len(image.shape) != 3):
+        print("Not Colored")
+    #image = cv2.cvtColor(image, cv2)
+
+    image = cv2.resize(image, (256, 256))
+
+    # save the image
+    cv2.imwrite(processedTrainDirectory + image_path)
