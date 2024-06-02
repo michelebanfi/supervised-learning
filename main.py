6d42c669f4b4fd7eb35d511c6c793ac3c9ff1ff0
lino <michi.banfi01@gmail.com>
2024-06-02 12:20:43 +0200

    working model

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
 import torch
 import torchvision.datasets
-
 from net import Net
 from torchsummary import summary
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
-train_on_gpu = torch.cuda.is_available()
-if not train_on_gpu:
-    print('CUDA is not available.  Training on CPU ...')
-else:
-    print('CUDA is available!  Training on GPU ...')
-device = torch.device("cuda:0" if train_on_gpu else "cpu")
-print(device)
+def main():
+    train_on_gpu = torch.cuda.is_available()
+    device = torch.device("cuda:0" if train_on_gpu else "cpu")
+    print("running on: ", device)
 
-net = Net()
-summary(net, (1, 28, 28))
-net.to(device)
+    net = Net(num_classes=251)
+    summary(net, (3, 256, 256))
+    net.to(device)
 
-lossOvertime = []
-accuracyOvertime = []
+    lossOvertime = []
+    accuracyOvertime = []
 
-# Define data transformations pipeline
-transforms = transforms.Compose([
-    transforms.ToTensor()
-])
+    # Define data transformations pipeline
+    transforms = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor()
+    ])
 
-trainSet = torchvision.datasets.ImageFolder(root='./Data/processedData/processed_train_set', transform=transforms)
+    trainSet = torchvision.datasets.ImageFolder(root='./Data/processedData/processed_train_set', transform=transforms)
 
-trainLoader = torch.utils.data.DataLoader(trainSet, batch_size=4, shuffle=True, num_workers=2)
+    trainLoader = DataLoader(trainSet, batch_size=64, shuffle=True, num_workers=2)
 
-epochs = 10
+    epochs = 1
 
-# for epoch in range(epochs):
-#     running_loss = 0.0
-#     for i, data in enumerate(trainLoader, 0):
-#         inputs, labels = data
-#         inputs, labels = inputs.to(device), labels.to(device)
-#         loss = net.trainStep(inputs, labels)
-#         running_loss += loss
-#     lossOvertime.append(running_loss)
-#     print(f"Epoch {epoch+1}, loss: {running_loss}")
+    for epoch in range(epochs):
+        running_loss = 0.0
+
+        for i, data in tqdm(enumerate(trainLoader), total=len(trainLoader)):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            loss = net.trainStep(inputs, labels)
+            running_loss += loss
+
+        lossOvertime.append(running_loss)
+        print(f"Epoch {epoch + 1}, loss: {running_loss}")
+
+        # save model
+        torch.save(net.state_dict(), f"model_{epoch}.pth")
+
+
+if __name__ == '__main__':
+    main()
--- net.py
+++ net.py
@@ -1,22 +1,73 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        # Define the feature extraction part of the network
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+
+        self.conv8 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.conv9 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.conv10 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+
+        self.conv11 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.conv12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.conv13 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+
+        # Define the classification part of the network
+        self.fc1 = nn.Linear(512 * 8 * 8, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, num_classes)
+
+        self.dropout = nn.Dropout()
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        self.criterion = nn.CrossEntropyLoss()
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 4 * 4)
+        # Feature extraction
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = F.relu(self.conv7(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv8(x))
+        x = F.relu(self.conv9(x))
+        x = F.relu(self.conv10(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv11(x))
+        x = F.relu(self.conv12(x))
+        x = F.relu(self.conv13(x))
+        x = self.pool(x)
+
+        # Flatten the tensor for the fully connected layers
+        x = x.view(-1, 512 * 8 * 8)
+
+        # Classification
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = F.relu(self.fc2(x))
+        x = self.dropout(x)
         x = self.fc3(x)
         return x
 
